583f78886523987b463fe00a95cd3b098b6b9d22
chahojin <chahojin1984@gmail.com>
2026-05-21 16:03:38 +0900

    no message

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from './App'
 import AuthCallbackPage from './pages/AuthCallbackPage'
 import LegalDocumentPage from './pages/LegalDocumentPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
@@ -29,6 +30,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<App />} />
         <Route path="/auth/callback" element={<AuthCallbackPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
         <Route
